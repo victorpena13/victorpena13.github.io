e85164c54cec0f0fd9541adb4b2a77b9a5757da3
victor pena <victor.pena1392@Gmail.com>
2022-02-05 18:10:22 -0600

    removed s from http

--- js/weather_map.js
+++ js/weather_map.js
@@ -44,7 +44,7 @@
         var userInput = event.result.center;
         var lon = userInput[0];
         var lat = userInput[1];
-        $.get("https://api.openweathermap.org/data/2.5/onecall", {
+        $.get("http://api.openweathermap.org/data/2.5/onecall", {
             APPID: openWeatherAPI_key,
             lat: lat,
             lon: lon,
